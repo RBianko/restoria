4446f7532a033cc270a4d235c8ebc8dacaa7bf57
RomanBinako <skayone1@gmail.com>
2023-01-12 19:30:41 +0300

    focus fix

--- lib/src/views/game/game.dart
+++ lib/src/views/game/game.dart
@@ -22,6 +22,7 @@ class Game extends StatelessWidget {
   Widget build(BuildContext context) {
     startBgmGameMusic();
     Vector2 mouseVector = Vector2(0, 0);
+    FocusNode gameFocus = FocusNode();
 
     updateMouseCoords(PointerEvent details) {
       final x = details.position.dx;
@@ -35,85 +36,90 @@ class Game extends StatelessWidget {
       child: LayoutBuilder(
         builder: (context, constraints) {
           MainMap.tileSize = max(constraints.maxHeight, constraints.maxWidth) / (kIsWeb ? 25 : 22);
+
           return MouseRegion(
             onHover: updateMouseCoords,
-            child: BonfireWidget(
-              showCollisionArea: true,
-              joystick: Platform.isAndroid && Platform.isIOS
-                  ? Joystick(
-                      directional: JoystickDirectional(
-                        spriteBackgroundDirectional: Sprite.load(
-                          'joystick/joystick_background.png',
-                        ),
-                        spriteKnobDirectional: Sprite.load('joystick/joystick_knob.png'),
-                        size: 100,
-                        isFixed: false,
-                      ),
-                      actions: [
-                        JoystickAction(
-                          actionId: HeroAttackType.attackMelee,
-                          sprite: Sprite.load('joystick/joystick_attack.png'),
-                          align: JoystickActionAlign.BOTTOM_RIGHT,
-                          size: 80,
-                          margin: const EdgeInsets.only(bottom: 50, right: 50),
-                        ),
-                        JoystickAction(
-                          actionId: HeroAttackType.attackRanged,
-                          sprite: Sprite.load('joystick/joystick_attack_range.png'),
-                          spriteBackgroundDirection: Sprite.load(
+            child: Focus(
+            onFocusChange: (_) => gameFocus.requestFocus(),
+              child: BonfireWidget(
+                showCollisionArea: true,
+                focusNode: gameFocus,
+                joystick: Platform.isAndroid && Platform.isIOS
+                    ? Joystick(
+                        directional: JoystickDirectional(
+                          spriteBackgroundDirectional: Sprite.load(
                             'joystick/joystick_background.png',
                           ),
-                          enableDirection: true,
-                          size: 50,
-                          margin: const EdgeInsets.only(bottom: 50, right: 160),
-                        )
-                      ],
-                    )
-                  : Joystick(
-                      keyboardConfig: KeyboardConfig(
-                        keyboardDirectionalType: KeyboardDirectionalType.wasd,
-                        acceptedKeys: [
-                          LogicalKeyboardKey.space,
-                          LogicalKeyboardKey.escape,
-                          LogicalKeyboardKey.tab,
-                          LogicalKeyboardKey.keyQ,
-                          LogicalKeyboardKey.keyE,
-                          LogicalKeyboardKey.keyR,
+                          spriteKnobDirectional: Sprite.load('joystick/joystick_knob.png'),
+                          size: 100,
+                          isFixed: false,
+                        ),
+                        actions: [
+                          JoystickAction(
+                            actionId: HeroAttackType.attackMelee,
+                            sprite: Sprite.load('joystick/joystick_attack.png'),
+                            align: JoystickActionAlign.BOTTOM_RIGHT,
+                            size: 80,
+                            margin: const EdgeInsets.only(bottom: 50, right: 50),
+                          ),
+                          JoystickAction(
+                            actionId: HeroAttackType.attackRanged,
+                            sprite: Sprite.load('joystick/joystick_attack_range.png'),
+                            spriteBackgroundDirection: Sprite.load(
+                              'joystick/joystick_background.png',
+                            ),
+                            enableDirection: true,
+                            size: 50,
+                            margin: const EdgeInsets.only(bottom: 50, right: 160),
+                          )
                         ],
+                      )
+                    : Joystick(
+                        keyboardConfig: KeyboardConfig(
+                          keyboardDirectionalType: KeyboardDirectionalType.wasd,
+                          acceptedKeys: [
+                            LogicalKeyboardKey.space,
+                            LogicalKeyboardKey.escape,
+                            LogicalKeyboardKey.tab,
+                            LogicalKeyboardKey.keyQ,
+                            LogicalKeyboardKey.keyE,
+                            LogicalKeyboardKey.keyR,
+                          ],
+                        ),
                       ),
-                    ),
-              player: PlayerHero(Vector2((8 * MainMap.tileSize), (5 * MainMap.tileSize)), getMouseVector),
-              interface: PlayerHeroInterface(),
-              map: WorldMapByTiled(
-                'tile/map.json',
-                forceTileSize: Vector2(MainMap.tileSize, MainMap.tileSize),
-                objectsBuilder: {
-                  'Goblin': (properties) => Goblin(properties.position),
+                player: PlayerHero(Vector2((8 * MainMap.tileSize), (5 * MainMap.tileSize)), getMouseVector),
+                interface: PlayerHeroInterface(),
+                map: WorldMapByTiled(
+                  'tile/map.json',
+                  forceTileSize: Vector2(MainMap.tileSize, MainMap.tileSize),
+                  objectsBuilder: {
+                    'Goblin': (properties) => Goblin(properties.position),
+                  },
+                ),
+                overlayBuilderMap: {
+                  'Bars': (context, game) => const Bars(),
+                  'GameOver': (context, game) => const GameOver(),
+                  'GameMenu': (context, game) => const GameMenu(),
+                  'MiniMap': (context, game) => MiniMap(
+                        game: game,
+                        margin: const EdgeInsets.all(10),
+                        borderRadius: BorderRadius.circular(10),
+                        size: Vector2.all(
+                          min(constraints.maxHeight, constraints.maxWidth) / 7,
+                        ),
+                        border: Border.all(color: Colors.white.withOpacity(0.5)),
+                      ),
+                  'HeroMenu': (context, game) => const HeroMenu(),
                 },
-              ),
-              overlayBuilderMap: {
-                'Bars': (context, game) => const Bars(),
-                'GameOver': (context, game) => const GameOver(),
-                'GameMenu': (context, game) => const GameMenu(),
-                'MiniMap': (context, game) => MiniMap(
-                      game: game,
-                      margin: const EdgeInsets.all(10),
-                      borderRadius: BorderRadius.circular(10),
-                      size: Vector2.all(
-                        min(constraints.maxHeight, constraints.maxWidth) / 7,
-                      ),
-                      border: Border.all(color: Colors.white.withOpacity(0.5)),
-                    ),
-                'HeroMenu': (context, game) => const HeroMenu(),
-              },
-              initialActiveOverlays: const [
-                'Bars',
-                'MiniMap',
-                'HeroMenu',
-              ],
-              cameraConfig: CameraConfig(
-                smoothCameraEnabled: true,
-                smoothCameraSpeed: 2,
+                initialActiveOverlays: const [
+                  'Bars',
+                  'MiniMap',
+                  'HeroMenu',
+                ],
+                cameraConfig: CameraConfig(
+                  smoothCameraEnabled: true,
+                  smoothCameraSpeed: 2,
+                ),
               ),
             ),
           );
